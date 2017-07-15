380c6a4e8b75a57b948a6b604b361c478353c702
Stefano Chang <schang@godaddy.com>
2017-07-15 16:55:07 -0700

    Add cost file

---
+++ cost.js
@@ -0,0 +1,302 @@
+[
+    {
+        "name": "International Institute for the Unification of Private Law ",
+    	"cost": 55411025.42
+	},
+    {
+        "name": "Bureau of Politico-Military Affairs",
+    	"cost": 5410025.02
+	},
+    {
+        "name": "Immediate Office of Assistant Secretary for Planning and Analysis",
+    	"cost": 838805021.70
+	},
+    {
+        "name": "Immediate Office of the Assistant Secretary for Special Education and Rehab Services",
+    	"cost": 469276292.87
+	},
+    {
+        "name": "Assistant Secretary for Public and Intergovernmental Affairs",
+    	"cost": 304558971.80
+	},
+    {
+        "name": "National Critical Materials Council",
+    	"cost": 589289256.56
+	},
+    {
+        "name": "Office of Regional Administrator, Region 2",
+    	"cost": 254065111.51
+	},
+    {
+        "name": "Defense Prisoner of War/Missing Personnel Office ",
+    	"cost": 417586786.41
+	},
+    {
+        "name": "Office of English Language Acquisition",
+    	"cost": 163023113.84
+	},
+    {
+        "name": "Air Force Review Boards Office",
+    	"cost": 1030215629.75
+	},
+    {
+        "name": "National Agricultural Statistics Service",
+    	"cost": 851408614.80
+	},
+    {
+        "name": "Field Operating Agencies of the Army Staff Resourced Through OA-22",
+    	"cost": 889208661.13
+	},
+    {
+        "name": "Field Office, Fernald",
+    	"cost": 210981940.14
+	},
+    {
+        "name": "Bureau of Educational and Cultural Affairs ",
+    	"cost": 686270100.00
+	},
+    {
+        "name": "Science & Energy",
+    	"cost": 17921768.39
+	},
+    {
+        "name": "Artistic Ambassador Advisory Committee ",
+    	"cost": 767512035.11
+	},
+    {
+        "name": "International Union of Physiological Sciences ",
+    	"cost": 802330015.19
+	},
+    {
+        "name": "Merit Systems Protection Board",
+    	"cost": 872178409.91
+	},
+    {
+        "name": "Health Resources and Services Administration",
+    	"cost": 859361375.19
+	},
+    {
+        "name": "Bureau of Population, Refugees and Migration ",
+    	"cost": 357716630.14
+	},
+    {
+        "name": "Immediate Office of the Secretary of State ",
+    	"cost": 1095729726.19
+	},
+    {
+        "name": "Chief of Staff",
+    	"cost": 105355055.05
+	},
+    {
+        "name": "U.S. Army Training and Doctrine Command ",
+    	"cost": 648805569.73
+	},
+    {
+        "name": "International Union of Microbiological Societies ",
+    	"cost": 736371435.88
+	},
+    {
+        "name": "Student Loan Marketing Association",
+    	"cost": 193572035.33
+	},
+    {
+        "name": "Family Support Administration",
+    	"cost": 770552478.01
+	},
+    {
+        "name": "Immediate Office of the Secretary of Transportation",
+    	"cost": 653701973.07
+	},
+    {
+        "name": "Office of Operations",
+    	"cost": 565380316.50
+	},
+    {
+        "name": "National Appeals Division",
+    	"cost": 196395756.20
+	},
+    {
+        "name": "Social Security Administration",
+    	"cost": 666946970.99
+	},
+    {
+        "name": "Air National Guard Units (Title 32)",
+    	"cost": 75141713.00
+	},
+    {
+        "name": "National Sheep Industry Improvement Center",
+    	"cost": 592388415.18
+	},
+    {
+        "name": "Rural Utilities Service",
+    	"cost": 918139349.18
+	},
+    {
+        "name": "Office of the Under Secretary for Global Affairs",
+    	"cost": 544439880.88
+	},
+    {
+        "name": "Bureau of International Organization Affairs ",
+    	"cost": 1011661989.19
+	},
+    {
+        "name": "Recovery Accountability and Transparency Board",
+    	"cost": 676126671.87
+	},
+    {
+        "name": "Peace Corps",
+    	"cost": 959376438.95
+	},
+    {
+        "name": "Assistant Secretary for Congressional and Intergovernmental Affairs",
+    	"cost": 739266642.19
+	},
+    {
+        "name": "Inter-American Development Bank",
+    	"cost": 933349051.18
+	},
+    {
+        "name": "United States Mint",
+    	"cost": 62899858.18
+	},
+    {
+        "name": "Federal Deposit Insurance Corporation",
+    	"cost": 226503040.19
+	},
+    {
+        "name": "Office of Policy Development",
+    	"cost": 1077956989.71
+	},
+    {
+        "name": "Office of Regional Administrator, Region 10",
+    	"cost": 563576323.19
+	},
+    {
+        "name": "Office of the Administrator of Epa",
+    	"cost": 427825958.00
+	},
+    {
+        "name": "Nuclear Waste Technical Review Board",
+    	"cost": 863431912.18
+	},
+    {
+        "name": "Advisory Commission on Electronic Commerce",
+    	"cost": 1129525201.91
+	},
+    {
+        "name": "Insular Affairs",
+    	"cost": 1106045978.11
+	},
+    {
+        "name": "Substance Abuse and Mental Health Services Administration",
+    	"cost": 277733146.88
+	},
+    {
+        "name": "Office of Strategic Planning and Management",
+    	"cost": 1141807767.41
+	},
+    {
+        "name": "Administration",
+    	"cost": 876673294.01
+	},
+    {
+        "name": "Office of Health, Safety and Security",
+    	"cost": 764282660.77
+	},
+    {
+        "name": "Air National Guard Units (Mobilization) (Title 5)",
+    	"cost": 317458150.11
+	},
+    {
+        "name": "Office of Human Resource Management",
+    	"cost": 180665211.10
+	},
+    {
+        "name": "Bartlesville Project Office",
+    	"cost": 424992337.20
+	},
+    {
+        "name": "Field Office, Richland",
+    	"cost": 661884533.10
+	},
+    {
+        "name": "Federal Executive Board-Kansas City, KS",
+    	"cost": 285305520.00
+	},
+    {
+        "name": "International Union of Crystallography",
+    	"cost": 920502920.77
+	},
+    {
+        "name": "Department of Agriculture",
+    	"cost": 586642949.15
+	},
+    {
+        "name": "Bureau of International Security and Nonproliferation",
+    	"cost": 1104478923.14
+	},
+    {
+        "name": "Federal Motor Carrier Safety Administration",
+    	"cost": 47920978.51
+	},
+    {
+        "name": "Deputy Assistant Secretary for Budget",
+    	"cost": 55411025.42
+	},
+    {
+        "name": "International Whaling Commission",
+    	"cost": 65716045.51
+	},
+    {
+        "name": "Office -Inspector General of the U.S. Postal Service ",
+    	"cost": 15741026.45
+	},
+    {
+        "name": "Immediate Office of the Under Secretary for Border and Transportation Security",
+    	"cost": 2817265391.44
+	},
+    {
+        "name": "Federal Mine Safety and Health Review Commission",
+    	"cost": 58124195.62
+	},
+    {
+        "name": "8th U.S. Army",
+    	"cost": 35412071.51
+	},
+    {
+        "name": "U.S. Army Research Laboratory Command",
+    	"cost": 35412065.14
+	},
+    {
+        "name": "Farm Service Agency",
+    	"cost": 7411105.41
+	},
+    {
+        "name": "Immediate Office of the Secretary",
+    	"cost": 44419015.22
+	},
+    {
+        "name": "Rural Housing Service",
+    	"cost": 315416027.07
+	},
+    {
+        "name": "Federal Energy Regulatory Commission",
+    	"cost": 152419785.36
+	},
+    {
+        "name": "Assistant Secretary for Domestic and International Energy Policy",
+    	"cost": 11421043.17
+	},
+    {
+        "name": "Office of the Secretary of Labor",
+    	"cost": 88151230.55
+	},
+    {
+        "name": "Special Projects Office",
+    	"cost": 43512351.77
+	},
+    {
+        "name": "Office of the Under Secretary of Health and Human Services",
+        "cost": 65177055.14
+    }
+]
